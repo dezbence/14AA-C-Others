122b778029b4a3761a682d16627fdb17bb2cec12
nyikoskata <nykata04@gmail.com>
2024-01-02 11:22:37 +0100

    files update

---
+++ VetCareConnect-Frontend/src/components/Calendar.vue
@@ -0,0 +1,72 @@
+<template>
+   
+    <Calendar v-model="date" inline class="text-center" />
+ 
+</template>
+
+<script setup>
+import Calendar from 'primevue/calendar';
+import { usePrimeVue } from 'primevue/config';
+
+import { ref } from "vue";
+
+const date = ref();
+const primevue = usePrimeVue();
+primevue.config.locale.firstDayOfWeek = 1;
+primevue.config.locale.dayNamesMin = ['V', 'H', 'K', 'Sz', 'Cs', 'P', 'Sz'];
+</script>
+
+<style lang="css">
+
+
+.p-datepicker {
+ background-color: #50B692;
+ color: #FFF;
+ border: none;
+
+}
+
+.p-datepicker .p-datepicker-header {
+ background-color: #50B692;
+ border-bottom: 5px solid #368267;
+}
+
+.p-datepicker-month,
+.p-datepicker-year {
+ color: #FFF;
+}
+
+.p-datepicker-next:hover,
+.p-datepicker-prev:hover {
+ background-color: transparent;
+}
+
+td span:hover {
+ background-color: #35a580;
+
+}
+
+.p-datepicker-today span {
+ background-color: #368267;
+ color: #FFF;
+}
+
+.p-datepicker .p-datepicker-header .p-datepicker-prev, .p-datepicker .p-datepicker-header .p-datepicker-next {
+ color: #FFF;
+ transform: scale(1.1);
+}
+
+.p-datepicker .p-datepicker-header .p-datepicker-prev:hover, .p-datepicker .p-datepicker-header .p-datepicker-next:hover {
+ transform: scale(1.4);
+ transition: ease-in-out 200ms;
+}
+
+@media (max-width: 576px) {
+ .p-datepicker table td > span {
+    width: 1.5rem !important;
+    height: 1.5rem !important;
+  } 
+ 
+}
+
+</style>
--- VetCareConnect-Frontend/src/main.js
+++ VetCareConnect-Frontend/src/main.js
@@ -1,14 +1,18 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+
+import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'primevue/resources/themes/saga-green/theme.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(PrimeVue)
+app.use(router).mount('#app')
 
-app.mount('#app')
