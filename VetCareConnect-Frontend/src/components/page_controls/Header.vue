f60f109dc5715c136fd30320c574a53279d4c9b6
JooBarnabas <joobarni@gmail.com>
2024-01-04 16:45:19 +0100

    Header, sidebar fix

--- VetCareConnect-Frontend/src/components/page_controls/Header.vue
+++ VetCareConnect-Frontend/src/components/page_controls/Header.vue
@@ -1,184 +1,184 @@
 <template>
+  <div class="heading">
+    <div class="hamburgerMenu">
+      <img @click="menuToggle()" id="toggleIcon"
+        :src="isMenuOpen ? 'src/assets/icons/close.svg' : 'src/assets/icons/menu.svg'">
+    </div>
 
-    <div class="heading"> 
-      <div class="hamburgerMenu" >
-        <img @click="menuToggle()" id="toggleIcon" :src="isMenuOpen ? 'src/assets/icons/close.svg' : 'src/assets/icons/menu.svg'">
+    <ul class="navigation">
+      <li>
+        <router-link to="/">
+          <img id="logo" src="../../assets/images/logo.png" />
+        </router-link>
+      </li>
+      <div class="routers" v-for="route in Routes">
+        <li><router-link :to="route.link">{{ route.name }}</router-link></li>
       </div>
-  
-      <ul class="navigation">
-        <li>
-          <router-link to="/">
-            <img id="logo" src="../../assets/images/logo.png"/>
-          </router-link>
-        </li>
-        <div class="routers" v-for="route in Routes">
-            <li><router-link :to= "route.link" >{{ route.name }}</router-link></li>
-          </div>
-      </ul>
-  
-      <div class="sign">
-        <span id="bejelentkezes"><router-link to="/bejelentkezes">Bejelentkezés</router-link></span>
-        <span id="regisztracio"><router-link to="/regisztracio">Regisztráció</router-link></span>
-      </div>
-      <div class="profile">
-        <img src="../../assets/icons/account_circle.svg">
-      </div>
+    </ul>
+
+    <div class="sign">
+      <span id="bejelentkezes"><router-link to="/bejelentkezes">Bejelentkezés</router-link></span>
+      <span id="regisztracio"><router-link to="/regisztracio">Regisztráció</router-link></span>
+    </div>
+
+    <div class="profile">
+      <img src="../../assets/icons/account_circle.svg">
+    </div>
   </div>
+
   <SideBar v-if="isMenuOpen"></SideBar>
-    
-  </template>
-  <router-view></router-view>
+</template>
   
-  <script setup>
-  import { RouterLink } from "vue-router";
-  import SideBar from "./SideBar.vue";
-  import { ref } from "vue";
-  
-  const Routes = [
-    { name: "Kezdőlap", link: "/" },
-    { name: "Időpont foglalás", link: "/idopontfoglalas" },
-    { name: "Kedvenceim", link: "/kedvenceim" },
-    { name: "Naptáram", link: "/naptaram" }
-  ]
-  
-      const isMenuOpen = ref(false);
-      
-      function menuToggle() {
-        isMenuOpen.value = !isMenuOpen.value;
-      }  
-  
-  </script>
-  <style scoped>
-  
-  .menuShow {
-    display: block;
-  }
-  
-  .menuHide {
-    display: none;
-  }
-  .heading {
-    background-color: #50b692;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 8px 8px;
-    width: 100%;
-    height: 60px;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    position: relative;
-    z-index: 99;
-    margin: 0;
-  }
-  
-  .navigation li {
-    padding: 1vh 1vw;
-    align-items: center;
-    justify-content: center;
-    display: flex;
-    display: inline-block;
-  }
-  
-  .navigation {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    list-style: none;
-    margin: 0;
-    padding-left: 0;
-  }
-  
-  .navigation .routers a {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  
-  .hamburgerMenu, .profile {
-    display: none;
-    cursor: pointer;
-  
-    filter: invert(100%);
-  }
-  
-  .hamburgerMenu:hover, .profile:hover {
-    transform: scale(1.1);
-    transition: 200ms;
-  }
-  .header-right {
-    display: flex;
-    align-items: center;
-  }
-  .heading a {
-    font-weight: 500;
-    color: #fff;
-    text-decoration: none;
-  }
-  
-  .heading a.router-link-exact-active {
-    color: #246951;
-  }
-  
-  .routers a.router-link-exact-active {
-    border-radius: 7px 7px 0 0;
-    height: 5px;
-    width: 110%;
-    box-shadow: 0 27px 0 0 #246951;
-  }
-  
-  .heading a:hover {
-    color: #246951;
-    transition: 300ms;
-  }
-  
-  .heading #logo {
-    height: 28px;
-    cursor: pointer;
-    z-index: 99;
-  }
-  
+<script setup>
+import { RouterLink } from "vue-router";
+import SideBar from "./SideBar.vue";
+import { ref } from "vue";
+
+const Routes = [
+  { name: "Kezdőlap", link: "/" },
+  { name: "Időpont foglalás", link: "/idopontfoglalas" },
+  { name: "Kedvenceim", link: "/kedvenceim" },
+  { name: "Naptáram", link: "/naptaram" }
+]
+
+const isMenuOpen = ref(false);
+
+function menuToggle() {
+  isMenuOpen.value = !isMenuOpen.value;
+}
+
+</script>
+
+<style scoped>
+.menuShow {
+  display: block;
+}
+
+.menuHide {
+  display: none;
+}
+
+.heading {
+  background-color: #50b692;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 8px;
+  width: 100%;
+  height: 60px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  position: relative;
+  z-index: 99;
+}
+
+.navigation li {
+  padding: 15px;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  display: inline-block;
+}
+
+.navigation {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  list-style: none;
+  margin: 0;
+  padding-left: 0;
+}
+
+.navigation .routers a {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.hamburgerMenu, .profile img {
+  display: none;
+  cursor: pointer;
+  filter: invert(100%);
+}
+
+.hamburgerMenu:hover, .profile:hover {
+  transform: scale(1.1);
+  transition: 200ms;
+}
+
+.heading a {
+  font-weight: 500;
+  color: #fff;
+  text-decoration: none;
+}
+
+.heading a.router-link-exact-active {
+  color: #246951;
+}
+
+.routers a.router-link-exact-active {
+  border-radius: 7px 7px 0 0;
+  height: 5px;
+  width: 110%;
+  box-shadow: 0 27px 0 0 #246951;
+}
+
+.heading a:hover {
+  color: #246951;
+  transition: 200ms;
+}
+
+.heading #logo {
+  height: 28px;
+  cursor: pointer;
+  z-index: 99;
+}
+
+.sign {
+  margin-right: 10px;
+  display: flex;
+  align-items: center;
+}
+
+.sign a {
+  padding: 10px;
+}
+
+#bejelentkezes {
+  background-color: #368267;
+  border-radius: 45px;
+  padding: 6px 8px;
+}
+
+@media (max-width: 991px) {
+
+  .routers,
   .sign {
-    margin-right: 1vh;
-    display: flex;
-    align-items: center;
+    font-size: 0.9rem;
   }
-  
-  .sign a {
-    padding: 1vh 1vw;
+
+  .routers a.router-link-exact-active {
+    box-shadow: 0 28px 0 0 #246951;
+    height: 4px;
   }
-  
-  #bejelentkezes {
-    background-color: #368267;
-    border-radius: 45px;
-    padding: 6px 8px;
+
+}
+
+@media (max-width: 891px) {
+  .routers {
+    display: none;
   }
-  
-  @media (max-width: 991px) {
-    .routers, .sign {
-      font-size: 0.9rem;
-    }
-  
-    .routers a.router-link-exact-active {
-      box-shadow: 0 28px 0 0 #246951;
-      height: 4px;
-    }
-  
+
+  .hamburgerMenu, #toggleIcon, .profile img {
+    display: block;
+    height: 30px;
   }
-  
-  @media (max-width: 891px) {
-      .routers {
-        display: none;
-      }  
-      .hamburgerMenu, .profile {
-        display: block;
-      }
-      .sign {
-        display: none;
-      }
-      .heading {
-        padding: 0.5vh 20px;
-      }
+
+  .sign {
+    display: none;
   }
-  
-  </style>
+
+  .heading {
+    padding: 5px 20px;
+  }
+}</style>
   
--- VetCareConnect-Frontend/src/components/page_controls/SideBar.vue
+++ VetCareConnect-Frontend/src/components/page_controls/SideBar.vue
@@ -1,104 +1,110 @@
 <template>
-    <div class="sideBar">
-      <div class="front">
-        <ul>
-          <div class="routers" v-for="route in Routes">
-            <li><router-link :to="route.link">{{ route.name }}</router-link></li>
-          </div>
-        </ul>
-  
-        <div class="sign">
-          <span id="bejelentkezes"><router-link to="/bejelentkezes">Bejelentkezés</router-link></span>
-          <span id="regisztracio"><router-link to="/regisztracio">Regisztráció</router-link></span>
+  <div class="sideBar">
+    <div class="front">
+      <ul>
+        <div class="routers" v-for="route in Routes">
+          <li><router-link :to="route.link">{{ route.name }}</router-link></li>
         </div>
+      </ul>
+
+      <div class="sign">
+        <button id="bejelentkezes"><router-link to="/bejelentkezes">Bejelentkezés</router-link></button>
+        <span id="regisztracio"><router-link to="/regisztracio">Regisztráció</router-link></span>
       </div>
-  
     </div>
-  </template>
+
+  </div>
+</template>
   
-  <script setup>
-  const Routes = [
-    { name: "Kezdőlap", link: "/" },
-    { name: "Időpont foglalás", link: "/idopontfoglalas" },
-    { name: "Kedvenceim", link: "/kedvenceim" },
-    { name: "Naptáram", link: "/naptaram" }
-  ]
-  </script>
+<script setup>
+const Routes = [
+  { name: "Kezdőlap", link: "/" },
+  { name: "Időpont foglalás", link: "/idopontfoglalas" },
+  { name: "Kedvenceim", link: "/kedvenceim" },
+  { name: "Naptáram", link: "/naptaram" }
+]
+</script>
   
-  <style scoped>
+<style scoped>
+.sideBar {
+  display: none;
+  background-color: #50B692;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  z-index: 98;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+.front {
+  margin-top: 80px;
+}
+
+ul {
+  list-style: none;
+  min-width: 300px;
+  width: 100%;
+  padding: 30px;
+}
+
+.routers li a {
+  text-decoration: none;
+  color: white;
+  font-weight: 500;
+}
+
+.routers li a.router-link-exact-active {
+  color: #246951;
+}
+
+.routers li {
+  margin-bottom: 15px;
+  font-size: 1rem;
+  border-bottom: 1px solid white;
+}
+
+.routers li a:hover,
+.sign a:hover {
+  color: #246951;
+}
+
+.sign {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+button a,
+span a {
+  font-weight: 500;
+  font-size: 1rem;
+  margin-top: 20px;
+  text-decoration: none;
+  color: white;
+  text-align: center;
+}
+
+button {
+  background-color: #368267;
+  border-radius: 45px;
+  padding: 6px 8px;
+  width: 200px;
+  border: 0;
+  margin-bottom: 10px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+
+@media (max-width: 891px) {
   .sideBar {
-    display: none;
-    background-color: #50B692;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    z-index: 98;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  
+    display: block;
   }
-  
-  .front {
-    margin-top: 80px;
+}
+
+@media (max-width: 450px) {
+  .sideBar {
+    right: 0;
   }
-  
-  ul {
-    list-style: none;
-    min-width: 300px;
-    width: 100%;
-    padding: 30px;
-  }
-  
-  .routers li a {
-    text-decoration: none;
-    color: white;
-    font-weight: 500;
-  }
-  
-  .routers li a.router-link-exact-active {
-    color: #246951;
-  }
-  
-  .routers li {
-    margin-bottom: 15px;
-    font-size: 1rem;
-    border-bottom: 1px solid white;
-  
-  }
-  
-  .sign span {
-  
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-  
-  }
-  
-  span a {
-    font-weight: 500;
-    font-size: 1rem;
-    margin-top: 20px;
-    text-decoration: none;
-    color: white;
-    text-align: center;
-  }
-  
-  #bejelentkezes a {
-    background-color: #368267;
-    border-radius: 45px;
-    padding: 6px 8px;
-    width: 200px;
-  }
-  
-  @media (max-width: 891px) {
-    .sideBar {
-      display: block;
-    }
-  }
-  
-  @media (max-width: 450px) {
-    .sideBar {
-      right: 0;
-    }
-  }
-  </style>
+}
+</style>
