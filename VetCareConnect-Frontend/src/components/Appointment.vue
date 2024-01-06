0cc9ac655719eea8deb622179f22a6ec46ba5383
dezbence <dezbence@gmail.com>
2024-01-06 18:04:43 +0100

    Appointment Component, Page

--- VetCareConnect-Frontend/src/components/Appointment.vue
+++ VetCareConnect-Frontend/src/components/Appointment.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-
+    <div class="card">
+        <p class="title">Bodri veszettség oltás</p>
+        <p class="vet">Dr. Állat Orvos 7623 Város, Utca 6.</p>
+        <p class="date">2023. október 7. 14:30</p>
     </div>
 </template>
 
@@ -8,6 +10,57 @@
 
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
+
+.card {
+        
+        border-radius: 7px;
+        background-color: #50B692;
+        justify-self: center;
+        width: 300px;
+        padding: 15px;
+        padding-left: 25px;
+        margin: 10px;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+p {
+        color: white;
+}
+
+.title {
+        font-weight: 700;
+        font-size: 18px;
+        margin: 0;
+}
+
+.vet {
+        font-size: 12px;
+}
+
+.date {
+        font-weight: 700;
+        font-size: 20px;
+        margin: 5px 0px 0px 0px;
+}
+
+@media (min-width: 1260px) {
+
+.card {
+        width: 400px;
+}
+.title {
+        font-size: 24px;
+}
+
+.vet {
+        font-size: 15px;
+}
+
+.date {
+        font-size: 25px;
+}
+
+}
 
 </style>
--- VetCareConnect-Frontend/src/views/user/AppointmentsPage.vue
+++ VetCareConnect-Frontend/src/views/user/AppointmentsPage.vue
@@ -1,14 +1,88 @@
 <template>
     <Header></Header>
-    Korábbi időpontjaim
+    <div>
+        <p class="nextAppointments">Következő időpontok:</p>
+    </div>
+    <div class="appointments">
+        <Appointment></Appointment>
+        <Appointment></Appointment>
+        <Appointment></Appointment>
+        <Appointment></Appointment>
+        <Appointment></Appointment>
+        
+    </div>
+    <div class="buttonContainer">
+        <router-link to="/idopontfoglalas">
+            <button class="newAppointmentButton">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="white" height="35" viewBox="0 -960 960 960" width="35">
+                    <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+                </svg>
+                Új időpont foglalás
+            </button>
+        </router-link>
+    </div>
     <Footer></Footer>
 </template>
 
 <script setup>
 import Header from '../../components/page_controls/Header.vue'
 import Footer from '../../components/page_controls/Footer.vue'
+import Appointment from '../../components/Appointment.vue'
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
+.appointments {
+    display: flex;
+    padding: 0;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    flex-wrap: wrap;
 
+}
+
+.nextAppointments {
+    color: #50B692;
+    font-weight: 700;
+    margin: 5px 0px 5px 40px;
+    font-size: 18px;
+}
+
+
+.buttonContainer {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+
+.newAppointmentButton {
+    background: #50B692;
+    font-size: 18px;
+    font-weight: 700;
+    border: 0;
+    margin: 5px;
+    padding: 15px;
+    width: 250px;
+    color: white;
+    border-radius: 7px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: 'Roboto', sans-serif;
+}
+
+.buttonContainer a {
+    text-decoration: none;
+}
+
+.newAppointmentButton svg {
+    margin-bottom: 1px;
+}
+
+@media (max-width: 959px) {
+    .appointments {
+        flex-direction: column;
+    }
+}
 </style>
