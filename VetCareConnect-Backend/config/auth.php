d5e53b81818d9397eb3e14e654e757a3a465f3b7
dezbence <dezbence@gmail.com>
2024-01-10 17:58:01 +0100

    auth update

--- VetCareConnect-Backend/config/auth.php
+++ VetCareConnect-Backend/config/auth.php
@@ -113,6 +113,18 @@ return [
             'expire' => 60,
             'throttle' => 60,
         ],
+        'owners' => [
+            'provider' => 'owners',
+            'table' => 'password_reset_tokens',
+            'expire' => 60,
+            'throttle' => 60,
+        ],
+        'vets' => [
+            'provider' => 'vets',
+            'table' => 'password_reset_tokens',
+            'expire' => 60,
+            'throttle' => 60,
+        ],
     ],
 
     /*
