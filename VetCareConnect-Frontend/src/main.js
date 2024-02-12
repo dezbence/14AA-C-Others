c060bbc90aa155207297e2132d7ebd8992172cc1
JooBarnabas <joobarni@gmail.com>
2024-02-12 14:13:34 +0100

    bootstrap removed

--- VetCareConnect-Frontend/src/main.js
+++ VetCareConnect-Frontend/src/main.js
@@ -8,8 +8,6 @@ import PrimeVue from 'primevue/config'
 import App from './App.vue'
 
 import 'primevue/resources/themes/saga-green/theme.css'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.bundle.js'
 import './assets/main.css'
 
 const app = createApp(App)
