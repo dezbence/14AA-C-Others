8d7eb6e9d93fd5e95e42e8ac2e311d01714a0199
nyikoskata <nykata04@gmail.com>
2024-02-26 13:38:33 +0100

    admin page design, api calls (get users)

---
+++ VetCareConnect-Frontend/src/views/AdminPage.vue
@@ -0,0 +1,156 @@
+<template>
+    <div class="data">
+        <h1>Állatorvosok</h1>
+        <DataTable class="table" v-model:selection="selectedPeople" v-model:editingRows="editingRows" :value="vets" editMode="row" dataKey="id"
+            @row-edit-save="onRowEditSave" :pt="{
+                table: { style: 'min-width: 50rem' },
+                column: {
+                    bodycell: ({ state }) => ({
+                        style: state['d_editing'] && 'padding-top: 0.6rem; padding-bottom: 0.6rem'
+                    })
+                }
+            }">
+            <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
+            <Column field="id" header="Azonosító" style="width: 20%"></Column>
+            <Column field="name" header="Név" style="width: 20%">
+                <template #editor="{ data, field }">
+                    <InputText v-model="data[field]" />
+                </template>
+            </Column>
+            <Column field="email" header="Email" style="width: 20%">
+                <template #editor="{ data, field }">
+                    <InputText v-model="data[field]" />
+                </template>
+            </Column>
+            <Column field="phone" header="Telefonszám" style="width: 20%">
+                <template #editor="{ data, field }">
+                    <InputText v-model="data[field]" />
+                </template>
+            </Column>
+            <Column field="" header="Alapértelmezett jelszó" style="width: 20%">
+                <template #editor="{ data, field }">
+                    <InputText v-model="data[field]" />
+                </template>
+            </Column>
+            <Column :rowEditor="true" style="width: 10%; min-width: 8rem" bodyStyle="text-align:center"></Column>
+        </DataTable>
+        <div class="btns">
+            <button class="btnStyle btnDelete" @click="deleteUser">Törlés</button>
+            <button class="btnStyle btnSave" @click="saveData">Mentés</button>
+        </div>
+    </div>
+    <div class="data">
+        <h1>Gazdák</h1>
+        <DataTable class="table" v-model:selection="selectedPeople" v-model:editingRows="editingRows" :value="owners" editMode="row" dataKey="id"
+            @row-edit-save="onRowEditSave" :pt="{
+                table: { style: 'min-width: 50rem' },
+                column: {
+                    bodycell: ({ state }) => ({
+                        style: state['d_editing'] && 'padding-top: 0.6rem; padding-bottom: 0.6rem'
+                    })
+                }
+            }">
+            <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
+            <Column field="id" header="Azonosító" style="width: 20%"></Column>
+            <Column field="name" header="Név" style="width: 20%">
+                <template #editor="{ data, field }">
+                    <InputText v-model="data[field]" />
+                </template>
+            </Column>
+            <Column field="email" header="Email" style="width: 20%">
+                <template #editor="{ data, field }">
+                    <InputText v-model="data[field]" />
+                </template>
+            </Column>
+            <Column field="phone" header="Telefonszám" style="width: 20%">
+                <template #editor="{ data, field }">
+                    <InputText v-model="data[field]" />
+                </template>
+            </Column>
+            <Column field="" header="Alapértelmezett jelszó" style="width: 20%">
+                <template #editor="{ data, field }">
+                    <InputText v-model="data[field]" />
+                </template>
+            </Column>
+            <Column :rowEditor="true" style="width: 10%; min-width: 8rem" bodyStyle="text-align:center"></Column>
+        </DataTable>
+        <div class="btns">
+            <button class="btnStyle btnDelete" @click="deleteUser">Törlés</button>
+            <button class="btnStyle btnSave" @click="saveData">Mentés</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import mainService from '@/services/mainservices';
+import InputText from 'primevue/inputtext';
+
+import { ref } from 'vue';
+import { storeToRefs } from "pinia";
+import { useUserStore } from "@/store/userstore";
+
+const { user } = storeToRefs(useUserStore());
+const editingRows = ref([]);
+const owners = ref([]);
+const vets = ref([]);
+const selectedPeople = ref();
+
+const onRowEditSave = (event) => {
+    let { newData, index } = event;
+
+    vets.value[index] = newData;
+};
+
+function saveData(){
+    // adatok módosítása
+}
+
+function deleteUser(){
+    // delete user
+}
+
+function getPeople(){
+    mainService.getAllOwner(user.value.token).then(resp =>{
+        owners.value = resp.data;
+        console.log(resp.data);
+    });
+    mainService.getAllVet(user.value.token).then(resp =>{
+        vets.value = resp.data;
+        console.log(resp.data);
+    });
+}
+getPeople();
+</script>
+
+<style lang="css" scoped>
+.btns {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+.btnStyle {
+    padding: 10px 20px;
+    margin: 20px 10px;
+}
+.btnDelete {
+    background-color: red;
+}
+.btnSave {
+    background-color: green;
+}
+h1 {
+    color: #246951;
+}
+.table {
+    max-width: 1000px;
+    margin: 10px 40px;
+}
+.data {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
