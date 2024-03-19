83d98b898d41470f45f51353362ae83c12ccaf83
JooBarnabas <joobarni@gmail.com>
2024-03-19 10:33:05 +0100

    Mail controller, verfication cont. delete

--- VetCareConnect-Backend/app/Http/Controllers/MailController.php
+++ VetCareConnect-Backend/app/Http/Controllers/MailController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Vet;
 use App\Models\Owner;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\AppointmentReminder;
@@ -23,8 +24,46 @@ class MailController extends BaseController
             }
 
         }
-
-
         Mail::to($user)->send(new AppointmentReminder());
+    }
+
+    public function verify($user_id, Request $request) {
+        if (!$request->hasValidSignature()) {
+            return $this->sendError('','Hibás vagy lejárt megerősítő link!');
+        }
+
+        $user = Vet::find($user_id);
+        if ($user == null) {
+            $user = Owner::find($user_id);
+        }
+
+        if ($user == null) {
+            return $this->sendError('','Hibás verifikáció!');
+        }
+
+        if (!$user->hasVerifiedEmail()) {
+            $user->markEmailAsVerified();
+        }
+
+        return $this->sendResponse('','Sikeres megerősítés!');
+    }
+
+    public function resend($email) {
+        $user = Vet::where('email', $email)->first();
+        if ($user == null) {
+            $user = Owner::where('email', $email)->first();
+        }
+
+        if ($user === null) {
+            return $this->sendError('','Nincs ilyen felhasználó!');
+        }
+
+        if ($user->hasVerifiedEmail()) {
+            return $this->sendError('','Az email már meg lett erősítve!');
+        }
+
+        $user->sendEmailVerificationNotification();
+
+        return $this->sendResponse('','Új megerősítő email sikeresen elküldve!');
     }
 }
