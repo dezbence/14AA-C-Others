f6c3eb543f33f70efa463897f0394397a9376aa9
JooBarnabas <joobarni@gmail.com>
2024-03-26 11:37:53 +0100

    email

--- VetCareConnect-Backend/resources/views/emails/appointment_booked.blade.php
+++ VetCareConnect-Backend/resources/views/emails/appointment_booked.blade.php
@@ -7,7 +7,9 @@ Időpont részletei:
 - **Állat neve:** {{ $appointmentData['pet_name'] }}
 - **Kezelés:** {{ $appointmentData['cure_type_name'] }}
 - **Állatorvos neve:** {{ $appointmentData['vet_name'] }}
-- **Rendelő címe:** {{ $appointmentData['vet_address'] }}
+- **Rendelő címe:** {{ $appointmentData['vet_postal']}} {{$appointmentData['vet_address']}}
 # Kezelés dátuma: {{ $appointmentData['date'] }}
+
+Kérjük legalább 10 perccel érkezzen hamarabb a helyszínre! Köszönjük!
 
 @endcomponent
