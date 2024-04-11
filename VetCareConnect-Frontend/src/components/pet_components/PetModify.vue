71d899b1e305cc48db6d5739b90bd1312a8ced7c
JooBarnabas <joobarni@gmail.com>
2024-04-11 10:18:49 +0200

    species

--- VetCareConnect-Frontend/src/components/pet_components/PetModify.vue
+++ VetCareConnect-Frontend/src/components/pet_components/PetModify.vue
@@ -66,7 +66,7 @@ const { user } = storeToRefs(useUserStore());
 const toast = useToast();
 const props = defineProps(['modifyPet']);
 
-const species = ['kutya', 'macska', 'hörcsög', 'nyúl', 'tengeri malac', 'görény', 'papagáj', 'teknős', 'ló', 'patkány', 'egér', 'sündisznó'];
+const species = ['kutya', 'macska', 'hörcsög', 'nyúl', 'papagáj', 'ló'];
 const genders = ['hím', 'nőstény'];
 
 
